ca57168001d47452d99a2c7762abae7cc0ba24aa
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-09-06 16:25:40 -0300

    Inicialização do projeto

---
+++ New2DUnityGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField]
+    float speed = 7;
+    private Rigidbody2D playerRigidbody;
+
+    void Start() {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update() {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(x, y, 0);
+        MoveCharacter(movement);
+    }
+
+    void MoveCharacter(Vector3 vector) {
+        playerRigidbody.MovePosition(transform.position + (vector * Time.deltaTime * speed));
+    }
+}
