153dcafa9f285526e2952eb37e66fd3ec52b8f8b
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-12-11 15:20:06 -0300

    nem meu primeiro, nem meu último commit resolvendo problemas de áudio

--- New2DUnityGame/Assets/Scripts/ScriptsMenu/StartMenu.cs
+++ New2DUnityGame/Assets/Scripts/ScriptsMenu/StartMenu.cs
@@ -7,29 +7,37 @@ using UnityEngine.SceneManagement;
 public class StartMenu : MonoBehaviour {
     public GameObject OptionsMenu;
     public GameObject ChoosePlayerMenu;
+    public AudioSource selectSound;
+
     public void PlayPVPMode() {
+        selectSound.Play();
         SceneManager.LoadScene("PVPMode");
     }
     public void PlayWavesMode() {
+        selectSound.Play();
         gameObject.SetActive(false);
         ChoosePlayerMenu.SetActive(true);
     }
 
     public void LoadWavesScene() {
+        selectSound.Play();
         SceneManager.LoadScene("Waves");
     }
 
     public void LoadWavesScene2P() {
+        selectSound.Play();
         SceneManager.LoadScene("Waves2P");
     }
 
 
     public void Options() {
+        selectSound.Play();
         gameObject.SetActive(false);
         OptionsMenu.SetActive(true);
     }
 
     public void Back() {
+        selectSound.Play();
         gameObject.SetActive(true);
         OptionsMenu.SetActive(false);
     }
